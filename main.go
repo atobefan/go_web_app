8b8c121341b2b15c9593adeb9b3b6277a15eae54
Fan Zhang <fanzhang@berkeley.edu>
2021-12-29 14:57:32 -0800

    basic hello world

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		n, err := fmt.Fprintf(w, "Hello world!")
-
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Bytes written: %d", n))
 	})
+
+	_ = http.ListenAndServe(":8080", nil)
 }
